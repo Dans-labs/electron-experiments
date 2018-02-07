a5faec15f004e1dbb26fc868c87bce18c9b7571f
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-02-07 15:19:01 +0100

    using lenses to modify state

--- redux-tutorial-typescript/src/ts/components/Layout.tsx
+++ redux-tutorial-typescript/src/ts/components/Layout.tsx
@@ -5,7 +5,7 @@ import {User} from "../model/user"
 import {connect, Dispatch} from "react-redux"
 import {AppState} from "../model/app"
 import {fetchUser} from "../actions/userActions"
-import {fetchTweets} from "../actions/tweetActions"
+import {fetchTweets, updateTweet} from "../actions/tweetActions"
 
 interface LayoutProps {
     user: User
@@ -21,12 +21,17 @@ class Layout extends Component<LayoutProps> {
 
     fetchTweets = () => this.props.dispatch(fetchTweets())
 
+    updateFirstTweet = () => this.props.dispatch(updateTweet("59f9fc94deab220100b5c92f", "Obama"))
+
     render() {
         const {user, tweets} = this.props
 
         const body = !tweets.length
             ? <button onClick={this.fetchTweets}>load tweets</button>
-            : <ul>{tweets.map(tweet => <li key={tweet.id}>{tweet.text}</li>)}</ul>
+            : <div>
+                <button onClick={this.updateFirstTweet}>update first tweet</button>
+                <ul>{tweets.map(tweet => <li key={tweet.id}>{tweet.text}</li>)}</ul>
+            </div>
 
         return <div>
             <h1>{user.name}</h1>
--- redux-tutorial-typescript/src/ts/reducers/userReducer.ts
+++ redux-tutorial-typescript/src/ts/reducers/userReducer.ts
@@ -1,6 +1,7 @@
-import {UserViewModel} from "../model/user"
-import {Reducer} from "redux"
+import {User, UserViewModel} from "../model/user"
+import {AnyAction, Reducer} from "redux"
 import {UserActionTypes} from "../constants/userConstants"
+import {Lens} from "monocle-ts"
 
 const initialState: UserViewModel = {
     user: {
@@ -11,22 +12,34 @@ const initialState: UserViewModel = {
     fetched: false,
 }
 
+const fetchingLens = Lens.fromProp<UserViewModel, 'fetching'>('fetching')
+const errorLens = new Lens<UserViewModel, [boolean, string|undefined]>(
+    s => [s.fetching, s.error],
+    a => s => ({...s, fetching: a[0], error: a[1]})
+)
+const receiveLens = new Lens<UserViewModel, [boolean, boolean, User]>(
+    s => [s.fetching, s.fetched, s.user],
+    a => s => ({...s, fetching: a[0], fetched: a[1], user: a[2]})
+)
+const name = Lens.fromPath<UserViewModel, 'user', 'name'>(['user', 'name'])
+const age = Lens.fromPath<UserViewModel, 'user', 'age'>(['user', 'age'])
+
 export const user: Reducer<UserViewModel> = (state: UserViewModel = initialState, action) => {
     switch (action.type) {
         case UserActionTypes.FETCH_USER: {
-            return {...state, fetching: true}
+            return fetchingLens.set(true)(state)
         }
         case UserActionTypes.FETCH_USER_REJECTED: {
-            return {...state, fetching: false, error: action.payload}
+            return errorLens.set([false, action.payload])(state)
         }
         case UserActionTypes.FETCH_USER_FULFILLED: {
-            return {...state, fetching: false, fetched: true, user: action.payload}
+            return receiveLens.set([false, true, action.payload])(state)
         }
         case UserActionTypes.SET_USER_NAME: {
-            return {...state, user: {...state.user, name: action.payload}}
+            return name.set(action.payload)(state)
         }
         case UserActionTypes.SET_USER_AGE: {
-            return {...state, user: {...state.user, age: action.payload}}
+            return age.set(action.payload)(state)
         }
     }
     return state
