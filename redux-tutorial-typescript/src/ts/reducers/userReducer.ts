f9ceab54ab5ea893766777e0ee7c8b2d71ee5533
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-02-07 15:59:29 +0100

    don't use Lens.fromPath!!!

--- redux-tutorial-typescript/src/ts/reducers/userReducer.ts
+++ redux-tutorial-typescript/src/ts/reducers/userReducer.ts
@@ -1,5 +1,5 @@
 import {User, UserViewModel} from "../model/user"
-import {AnyAction, Reducer} from "redux"
+import {Reducer} from "redux"
 import {UserActionTypes} from "../constants/userConstants"
 import {Lens} from "monocle-ts"
 
@@ -12,6 +12,7 @@ const initialState: UserViewModel = {
     fetched: false,
 }
 
+// TODO it turns out that Lens.fromPath slows down the performance of IntelliJ dramatically! DON'T use that method!!!
 const fetchingLens = Lens.fromProp<UserViewModel, 'fetching'>('fetching')
 const errorLens = new Lens<UserViewModel, [boolean, string|undefined]>(
     s => [s.fetching, s.error],
@@ -21,24 +22,30 @@ const receiveLens = new Lens<UserViewModel, [boolean, boolean, User]>(
     s => [s.fetching, s.fetched, s.user],
     a => s => ({...s, fetching: a[0], fetched: a[1], user: a[2]})
 )
-const name = Lens.fromPath<UserViewModel, 'user', 'name'>(['user', 'name'])
-const age = Lens.fromPath<UserViewModel, 'user', 'age'>(['user', 'age'])
+const userLens = Lens.fromProp<UserViewModel, 'user'>('user')
+const name = userLens.compose(Lens.fromProp<User, 'name'>('name'))
+const age = userLens.compose(Lens.fromProp<User, 'age'>('age'))
 
 export const user: Reducer<UserViewModel> = (state = initialState, action) => {
     switch (action.type) {
         case UserActionTypes.FETCH_USER: {
+            // return {...state, fetching: true}
             return fetchingLens.set(true)(state)
         }
         case UserActionTypes.FETCH_USER_REJECTED: {
+            // return {...state, fetching: false, error: action.payload}
             return errorLens.set([false, action.payload])(state)
         }
         case UserActionTypes.FETCH_USER_FULFILLED: {
+            // return {...state, fetching: false, fetched: true, user: action.payload}
             return receiveLens.set([false, true, action.payload])(state)
         }
         case UserActionTypes.SET_USER_NAME: {
+            // return {...state, user: {...state.user, name: action.payload}}
             return name.set(action.payload)(state)
         }
         case UserActionTypes.SET_USER_AGE: {
+            // return {...state, user: {...state.user, age: action.payload}}
             return age.set(action.payload)(state)
         }
     }
