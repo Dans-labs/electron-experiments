fd1f832ab31121f79919621aadc8b376d2efc26d
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-02-08 17:33:02 +0100

    split webpack stuff into dev and prod configuration

---
+++ redux-tutorial-typescript/deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# this is just a stub file for now. Here we could do things like package an rpm or stuff like that...
+echo "hello world"
---
+++ redux-tutorial-typescript/webpack/prod.config.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const path = require('path');
+const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const baseConfig = require('./base.config.js');
+
+module.exports = merge(baseConfig, {
+    output: {
+        path: path.join(process.cwd(), 'build'),
+        filename: '[name].bundle.[chunkhash].js',
+    },
+
+    plugins: [
+        // Clear out `build` directory between builds
+        new CleanWebpackPlugin(['build'], {
+            root: process.cwd(),
+        }),
+        // Minify JS
+        new UglifyJsPlugin({
+            test: /\.[j|t]sx?($|\?)/i,
+            sourceMap: true,
+            parallel: true,
+            uglifyOptions: {
+                ecma: 6,
+                // compress: true,
+                mangle: true,
+            },
+            extractComments: true,
+        }),
+    ],
+});
