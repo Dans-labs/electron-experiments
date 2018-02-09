7b99391029f08fe23a3203f6e903f810ef50393c
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-02-09 13:00:29 +0100

    resolved issue with lens/traversal over list

--- redux-tutorial-typescript/src/ts/reducers/tweetsReducer.ts
+++ redux-tutorial-typescript/src/ts/reducers/tweetsReducer.ts
@@ -1,7 +1,8 @@
 import {Tweet, TweetViewModel} from "../model/tweet"
 import {TweetActionTypes} from "../constants/tweetsConstants"
 import {Reducer} from "redux"
-import {Lens} from "monocle-ts"
+import {Lens, fromTraversable, Traversal} from "monocle-ts"
+import { array } from 'fp-ts/lib/Array'
 
 const initialState: TweetViewModel = {
     tweets: [],
@@ -10,16 +11,16 @@ const initialState: TweetViewModel = {
 }
 
 const fetchingLens = Lens.fromProp<TweetViewModel, 'fetching'>('fetching')
-// const errorLens = new Lens<TweetViewModel, [boolean, string|undefined]>(
-//     s => [s.fetching, s.error],
-//     a => s => ({...s, fetching: a[0], error: a[1]})
-// )
-// const receiveLens = new Lens<TweetViewModel, [boolean, boolean, Tweet[]]>(
-//     s => [s.fetching, s.fetched, s.tweets],
-//     a => s => ({...s, fetching: a[0], fetched: a[1], tweets: a[2]})
-// )
+const errorLens = new Lens<TweetViewModel, [boolean, string|undefined]>(
+    s => [s.fetching, s.error],
+    a => s => ({...s, fetching: a[0], error: a[1]})
+)
+const receiveLens = new Lens<TweetViewModel, [boolean, boolean, Tweet[]]>(
+    s => [s.fetching, s.fetched, s.tweets],
+    a => s => ({...s, fetching: a[0], fetched: a[1], tweets: a[2]})
+)
 const tweetsLens = Lens.fromProp<TweetViewModel, 'tweets'>('tweets')
-const tweetTextLens = Lens.fromProp<Tweet, 'text'>('text')
+const tweetLens = tweetsLens.composeTraversal(fromTraversable(array)<Tweet>())
 
 export const tweets: Reducer<TweetViewModel> = (state = initialState, action) => {
     switch (action.type) {
@@ -28,16 +29,16 @@ export const tweets: Reducer<TweetViewModel> = (state = initialState, action) =>
             return fetchingLens.set(true)(state)
         }
         case TweetActionTypes.FETCH_TWEETS_REJECTED: {
-            return {...state, fetching: false, error: action.payload}
-            // return errorLens.set([false, action.payload])(state)
+            // return {...state, fetching: false, error: action.payload}
+            return errorLens.set([false, action.payload])(state)
         }
         case TweetActionTypes.FETCH_TWEETS_FULFILLED: {
-            return {...state, fetched: false, fetching: true, tweets: action.payload.data}
-            // return receiveLens.set([false, true, action.payload.data])(state)
+            // return {...state, fetched: false, fetching: true, tweets: action.payload.data}
+            return receiveLens.set([false, true, action.payload.data])(state)
         }
         case TweetActionTypes.ADD_TWEET: {
-            return {...state, tweets: [...state.tweets, action.payload]}
-            // return tweetsLens.modify(tweets => [...tweets, action.payload])(state)
+            // return {...state, tweets: [...state.tweets, action.payload]}
+            return tweetsLens.modify(tweets => [...tweets, action.payload])(state)
         }
         case TweetActionTypes.UPDATE_TWEET: {
             // const { id } = action.payload
@@ -46,11 +47,11 @@ export const tweets: Reducer<TweetViewModel> = (state = initialState, action) =>
             // newTweets[tweetToUpdate] = action.payload;
             // return {...state, tweets: newTweets}
             const { id, text } = action.payload
-            return tweetsLens.modify(tweets => tweets.map(tweet => (tweet.id === id ? tweetTextLens.set(text)(tweet) : tweet)))(state)
+            return tweetLens.modify(tweet => (tweet.id === id ? {...tweet, text: text} : tweet))(state)
         }
         case TweetActionTypes.DELETE_TWEET: {
             // return {...state, tweets: state.tweets.filter(tweet => tweet.id !== action.payload)}
-            return tweetsLens.modify(tweets => state.tweets.filter(tweet => tweet.id !== action.payload))(state)
+            return tweetsLens.modify(tweets => tweets.filter(tweet => tweet.id !== action.payload))(state)
         }
     }
     return state;
