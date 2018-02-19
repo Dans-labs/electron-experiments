acc49354e89ef6a9136f5e2abc00da09d637e425
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-02-19 08:24:00 +0100

    first form example with validation

--- redux-form-tutorial/src/main/typescript/client.tsx
+++ redux-form-tutorial/src/main/typescript/client.tsx
@@ -3,8 +3,10 @@ import * as ReactDOM from "react-dom"
 import {Provider} from "react-redux"
 import store from "./store"
 import Header from "./components/Header"
+import DemoForm from "./components/DemoForm"
+import "../resources/css/styling"
 
 ReactDOM.render(
-    <Provider store={store}><Header/></Provider>,
-    document.getElementById('app')
+    <Provider store={store}><div><Header/><DemoForm/></div></Provider>,
+    document.getElementById('app'),
 )
