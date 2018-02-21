a75b87f7d15233687b24b21b11a61ea469dfaa38
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-02-21 12:01:44 +0100

    simplify date picker component

--- redux-form-tutorial/src/main/typescript/lib/form.tsx
+++ redux-form-tutorial/src/main/typescript/lib/form.tsx
@@ -5,22 +5,27 @@ import {BaseFieldProps, WrappedFieldProps} from "redux-form/lib/Field"
 import DatePicker from 'react-datepicker'
 import * as moment from "moment"
 import {Moment} from "moment"
+import {Simulate} from "react-dom/test-utils"
+import input = Simulate.input
 
 type FieldProps = WrappedFieldProps & BaseFieldProps
 type renderer = (input: WrappedFieldInputProps, label?: string, rest?: any) => JSX.Element
 
-const createRenderer = (renderer: renderer) => ({input, meta, label, name, ...rest}: FieldProps) => {
-    const changed = (meta as any).changed
-    const hasError = meta.error && changed
+function createRenderer <T> (renderer: renderer) {
+    return (props: FieldProps & T) => {
+        const {input, meta, label} = props
+        const changed = (meta as any).changed
+        const hasError = meta.error && changed
 
-    return <div className={[
-        hasError ? 'error' : '',
-        meta.active ? 'active' : '',
-    ].join(' ')}>
-        <label>{label}</label>
-        {renderer(input, label, rest)}
-        {hasError && <span>{meta.error}</span>}
-    </div>
+        return <div className={[
+            hasError ? 'error' : '',
+            meta.active ? 'active' : '',
+        ].join(' ')}>
+            <label>{label}</label>
+            {renderer(input, label, props)}
+            {hasError && <span>{meta.error}</span>}
+        </div>
+    }
 }
 
 export const RenderInput = createRenderer((input, label) => <input {...input} placeholder={label}/>)
@@ -64,50 +69,28 @@ export class RenderRadio extends Component<FieldProps & RadioProps> {
 }
 
 interface DatePickerProps {
+    dateFormat: string
     minDate?: Moment
     maxDate?: Moment
 }
 
-export class RenderDatePicker extends Component<DatePickerProps & FieldProps> {
-    constructor(props: DatePickerProps & FieldProps) {
-        super(props)
-    }
+export const RenderDatePicker = createRenderer<DatePickerProps>((input, label, {minDate, maxDate, dateFormat}) => {
+    return <DatePicker {...input}
+                       dateFormat={dateFormat}
+                       minDate={minDate}
+                       maxDate={maxDate}
 
-    dateFormat = "DD-MM-YYYY"
+                       todayButton="Today"
+                       disabledKeyboardNavigation
+                       isClearable
 
-    handleChange = (date: Moment) => {
-        this.props.input.onChange(date ? moment(date).format(this.dateFormat) : "")
-    }
+                       showYearDropdown
+                       scrollableYearDropdown
+                       yearDropdownItemNumber={15}
 
-    render() {
-        const {input, label, meta, minDate, maxDate} = this.props
-        const changed = (meta as any).changed
-        const hasError = meta.error && changed
-
-        return <div className={[
-            hasError ? 'error' : '',
-            meta.active ? 'active' : '',
-        ].join(' ')}>
-            <label>{label}</label>
-            <DatePicker {...input}
-                        dateFormat={this.dateFormat}
-                        minDate={minDate}
-                        maxDate={maxDate}
-
-                        todayButton="Today"
-                        disabledKeyboardNavigation
-                        isClearable
-
-                        showYearDropdown
-                        scrollableYearDropdown
-                        yearDropdownItemNumber={15}
-
-                        placeholderText={label}
-                        selected={input.value ? moment(input.value, this.dateFormat) : null}
-                        onChange={this.handleChange}>
-                <div style={{color: 'red'}}>This date must be in the future!</div>
-            </DatePicker>
-            {hasError && <span>{meta.error}</span>}
-        </div>
-    }
-}
+                       placeholderText={label}
+                       selected={input.value ? moment(input.value, dateFormat) : null}
+                       onChange={date => input.onChange(date ? moment(date).format(dateFormat) : "")}>
+        <div style={{color: 'red'}}>This date must be in the future!</div>
+    </DatePicker>
+})
