513a9bdb05194e9d83f02b2fcc842c5b83f7e292
Richard van Heest <richard.van.heest@dans.knaw.nl>
2018-02-27 16:16:13 +0100

    improve redux for foldable state

--- redux-form-tutorial/src/main/typescript/actions/foldableActions.ts
+++ redux-form-tutorial/src/main/typescript/actions/foldableActions.ts
@@ -1,5 +1,10 @@
 import {ReduxAction} from "../util"
 
+export const register: (id: string, open: boolean) => ReduxAction<[string, boolean]> = (id, open) => ({
+    type: "REGISTER_FOLDABLE",
+    payload: [id, open]
+})
+
 export const toggle: (id: string) => ReduxAction<string> = id => ({
     type: "TOGGLE_FOLDABLE",
     payload: id
